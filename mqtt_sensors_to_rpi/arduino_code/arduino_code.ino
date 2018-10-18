5125df734f4bdee70f0912cb42fd709acef24dd7
Peerawit-SC <peerawit.sc@gmail.com>
2018-10-19 06:49:52 +0700

    change file

--- mqtt_sensors_to_rpi/arduino_code/arduino_code.ino
+++ mqtt_sensors_to_rpi/arduino_code/arduino_code.ino
@@ -1,34 +1,51 @@
-float I1 = 400.56;
+float I1 = 1.11;
+float I2 = 2.22;
+float I3 = 3.33;
+float I4 = 4.44;
 
-float I2 = 567.89;
+float P1 = 10.11;
+float P2 = 20.22;
+float P3 = 30.33;
+float P4 = 40.44;
+
+float S1 = 100.11;
+float S2 = 200.22;
+float S3 = 300.33;
+float S4 = 400.44;
 
 #include <SoftwareSerial.h>
 
 SoftwareSerial NanoSerial(3, 2); // RX | TX
 
-void setup()
-
-{  
-
+void setup(){  
   Serial.begin(9600);
-
   NanoSerial.begin(4800);           
-
 }
 
-void loop() {
-
-  Serial.print(I1); 
-  Serial.print("\t");
-  Serial.println(I2);
-//  NanoSerial.print(110);
-//  NanoSerial.print("\t");
-  NanoSerial.print(I1);
+void loop(){
+  NanoSerial.print(I1); 
   NanoSerial.print('\t');
   NanoSerial.print(I2); 
-//  NanoSerial.print(1234567890);
+  NanoSerial.print('\t');
+  NanoSerial.print(I3); 
+  NanoSerial.print('\t');
+  NanoSerial.print(I4); 
+  NanoSerial.print('\t');
+  NanoSerial.print(P1); 
+  NanoSerial.print('\t');
+  NanoSerial.print(P2); 
+  NanoSerial.print('\t');
+  NanoSerial.print(P3); 
+  NanoSerial.print('\t');
+  NanoSerial.print(P4);
+  NanoSerial.print('\t');
+  NanoSerial.print(S1); 
+  NanoSerial.print('\t');
+  NanoSerial.print(S2); 
+  NanoSerial.print('\t');
+  NanoSerial.print(S3); 
+  NanoSerial.print('\t');
+  NanoSerial.print(S4);
   NanoSerial.print('\n');
-
   delay(500);
-
 }
