b7561229cf3701c9f757af55156267eee1fa55a6
Peerawit-SC <peerawit.sc@gmail.com>
2019-01-14 19:58:21 +0700

    change

--- rpi_code/startup_rpi.py
+++ rpi_code/startup_rpi.py
@@ -2,19 +2,20 @@ import time
 import threading
 import os
 
-def startprgm(i):
-    print "Running thread %d" % i
-    if (i == 0):
-        time.sleep(1)
-        print('Running: insert_influxdb.py')
-        os.system("sudo python /home/pi/Desktop/project_code/smart_meter_nilm_finalproject/rpi_code/insert_influxdb.py")
-    elif (i == 1):
-        print('Running: query_to_firebase.py')
-        time.sleep(1)
-        #os.system("sudo python /home/pi/Desktop/project_code/smart_meter_nilm_finalproject/rpi_code/query_to_firebase.py")
-    else:
-        pass
+os.system("sudo python /home/pi/Desktop/project_code/smart_meter_nilm_finalproject/rpi_code/insert_influxdb.py")
+# def startprgm(i):
+#     print "Running thread %d" % i
+#     if (i == 0):
+#         time.sleep(1)
+#         print('Running: insert_influxdb.py')
+#         os.system("sudo python /home/pi/Desktop/project_code/smart_meter_nilm_finalproject/rpi_code/insert_influxdb.py")
+#     elif (i == 1):
+#         print('Running: query_to_firebase.py')
+#         time.sleep(1)
+#         #os.system("sudo python /home/pi/Desktop/project_code/smart_meter_nilm_finalproject/rpi_code/query_to_firebase.py")
+#     else:
+#         pass
 
-for i in range(2):
-    t = threading.Thread(target=startprgm, args=(i,))
-    t.start()
+# for i in range(2):
+#     t = threading.Thread(target=startprgm, args=(i,))
+#     t.start()
