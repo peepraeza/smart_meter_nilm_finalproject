132c0aa32fc882b356da162f112686c7f7d3d30e
Peerawit-SC <peerawit.sc@gmail.com>
2019-01-15 00:07:45 +0700

    change

--- rpi_code/query_to_firebase2.py
+++ rpi_code/query_to_firebase2.py
@@ -43,57 +43,14 @@ def on_message(client, userdata, msg):
 		{
 		    "measurement": "energy_monitor",
 		    "time": current_time,
-		    "fields": {
-		        "irms1": float(pieces[1]),
-		        "irms2": float(pieces[2]),
-		        "irms3": float(pieces[3]),
-		        "irms4": float(pieces[4]),
-
-		        "realpower1": float(pieces[5]),
-		        "realpower2": float(pieces[6]),
-		        "realpower3": float(pieces[7]),
-		        "realpower4": float(pieces[8]),
-
-		        "apparentpower1": float(pieces[9]),
-		        "apparentpower2": float(pieces[10]),
-		        "apparentpower3": float(pieces[11]),
-		        "apparentpower4": float(pieces[12])
-		    }
+		    "fields": json_firebase
 		}]
 		print("firebase_time", time_unix)
 		client_db.write_points(json_influx)
 		firebases.post('/energy',json_firebase)
-		time.sleep(1)
+		time.sleep(4)
 	else:
 		print("Miss some data")
-
-# def insertdb(message):
-# 	time_unix = int(time.time())
-# 	pieces = message.split(',')
-# 	if(pieces[0] == "START" and pieces[-1] == "END"):
-# 		firebases = firebase.FirebaseApplication("https://data-log-fb39d.firebaseio.com/")
-# 		json_body = {
-# 			"time": time_unix,
-# 	        "I1": float(pieces[1]),
-# 	        "I2": float(pieces[2]),
-# 	        "I3": float(pieces[3]),
-# 	        "I4": float(pieces[4]),
-
-# 	        "P1": float(pieces[5]),
-# 	        "P2": float(pieces[6]),
-# 	        "P3": float(pieces[7]),
-# 	        "P4": float(pieces[8]),
-
-# 	        "S1": float(pieces[9]),
-# 	        "S2": float(pieces[10]),
-# 	        "S3": float(pieces[11]),
-# 	        "S4": float(pieces[12])  
-# 			}
-# 		print("firebase_time", time_unix)
-# 		firebases.post('/energy',json_body)
-# 		time.sleep(3)
-# 	else:
-# 		print("Miss some data")
 
 client = mqtt.Client()
 client.on_connect = on_connect
--- rpi_code/test_time.py
+++ rpi_code/test_time.py
@@ -1,13 +1,10 @@
-from firebase import firebase
-import time
-import threading
-from datetime import datetime
-time_now = 0
-time_now = int(time.time())
-firebases = firebase.FirebaseApplication("https://data-log-fb39d.firebaseio.com/")
-while(True):
-	time_now = int(time.time())
-	json_body = {"time" : time_now}
-	firebases.post('/time_now',json_body)
-	print(time_now)
-	time.sleep(2)
+p = {"irms1": 2, "irms2": 4, "irms3": 1, "irms4": 4}
+json_influx = [
+		{
+		    "measurement": "energy_monitor",
+		    "time": 90,
+		    "fields" : p
+		}]
+
+print(json_influx)
+print(p)
