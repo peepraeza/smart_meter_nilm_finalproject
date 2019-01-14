96f19e732beb1cdaaa55d8e97830109f4351e120
Peerawit-SC <peerawit.sc@gmail.com>
2019-01-14 22:49:44 +0700

    change

--- rpi_code/query_to_firebase2.py
+++ rpi_code/query_to_firebase2.py
@@ -12,13 +12,11 @@ def on_connect(client, userdata, flags, rc):
 	client.subscribe("esp8266")
 
 def on_message(client, userdata, msg):
-	insertdb(str(msg.payload))
-
-def insertdb(message):
+	message = str(msg.payload)
 	time_unix = int(time.time())
 	pieces = message.split(',')
+	firebases = firebase.FirebaseApplication("https://data-log-fb39d.firebaseio.com/")
 	if(pieces[0] == "START" and pieces[-1] == "END"):
-		firebases = firebase.FirebaseApplication("https://data-log-fb39d.firebaseio.com/")
 		json_body = {
 			"time": time_unix,
 	        "I1": float(pieces[1]),
@@ -38,9 +36,37 @@ def insertdb(message):
 			}
 		print("firebase_time", time_unix)
 		firebases.post('/energy',json_body)
-		time.sleep(3)
+		time.sleep(5)
 	else:
 		print("Miss some data")
+
+# def insertdb(message):
+# 	time_unix = int(time.time())
+# 	pieces = message.split(',')
+# 	if(pieces[0] == "START" and pieces[-1] == "END"):
+# 		firebases = firebase.FirebaseApplication("https://data-log-fb39d.firebaseio.com/")
+# 		json_body = {
+# 			"time": time_unix,
+# 	        "I1": float(pieces[1]),
+# 	        "I2": float(pieces[2]),
+# 	        "I3": float(pieces[3]),
+# 	        "I4": float(pieces[4]),
+
+# 	        "P1": float(pieces[5]),
+# 	        "P2": float(pieces[6]),
+# 	        "P3": float(pieces[7]),
+# 	        "P4": float(pieces[8]),
+
+# 	        "S1": float(pieces[9]),
+# 	        "S2": float(pieces[10]),
+# 	        "S3": float(pieces[11]),
+# 	        "S4": float(pieces[12])  
+# 			}
+# 		print("firebase_time", time_unix)
+# 		firebases.post('/energy',json_body)
+# 		time.sleep(3)
+# 	else:
+# 		print("Miss some data")
 
 client = mqtt.Client()
 client.on_connect = on_connect
